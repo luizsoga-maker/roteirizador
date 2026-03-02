d031bead33632917414a7099da1df7900cefb89c
[dyad] <luiz.soga@gmail.com>
2026-03-02 00:24:34 -0300

    [dyad] wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Index } from "./pages/Index";
+import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Routing from "./pages/Routing";
 
 import "./App.css";
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/route" element={<Routing />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/route" element={<Routing />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,15 @@
 "use client";
 
-export default function Logo() {
+interface LogoProps {
+  className?: string;
+}
+
+export default function Logo({ className }: LogoProps) {
   return (
     <img
       src="/placeholder.svg"
       alt="Logo"
-      className="w-24 h-24 object-contain mx-auto d-block"
+      className={`w-24 h-24 object-contain mx-auto d-block ${className}`}
     />
   );
 }
